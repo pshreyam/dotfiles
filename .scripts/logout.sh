85aa6155a43afbf75f09e216de1e4ca0d77013d7
Shreyam Pokharel <pshreyam@gmail.com>
2021-10-04 11:33:45 +0545

    fix: leave out default config in rofi

--- .scripts/logout.sh
+++ .scripts/logout.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-res="$(echo -e "Yes\nNo" | rofi -dmenu -p "Are you sure you want to log off" -i)"
+res="$(echo -e "Yes\nNo" | rofi -no-default-config -dmenu -p "Are you sure you want to log off" -i)"
 
 if [[ "$res" == "Yes" ]]; then
     bspc quit
