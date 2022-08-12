e4027e6a6a9f8803ddfc7b56dd02d1f3704ad2d5
Shreyam Pokharel <pshreyam@gmail.com>
2022-08-12 11:11:54 +0545

    Update config files
    - Update polybar interface to transparent in order to blend with the wallpaper
    - Update sxhkd file to modify lock/powermenu keybindings and remove keybindings for surf
    - Update powermenu script to incorporate screenlock feature
    - Change i3lock to betterlockscreen

--- .scripts/powermenu/powermenu.sh
+++ .scripts/powermenu/powermenu.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-res="$(printf " Logout\n Shutdown\n Reboot" | rofi -no-default-config -dmenu -p "Power Menu ›" -i)"
+res="$(printf " Lock\n Logout\n Shutdown\n Reboot" | rofi -no-default-config -dmenu -p "Power Menu ›" -i)"
 
-if [ "$res" == " Logout" ]; then
+if [ "$res" == " Lock" ]; then
+    betterlockscreen -l blur
+elif [ "$res" == " Logout" ]; then
     ~/.scripts/powermenu/logout.sh
 elif [ "$res" == " Shutdown" ]; then
     ~/.scripts/powermenu/shutdown.sh
